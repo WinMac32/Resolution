b9756da5df95b7aaa27414a9a7b17c8fd75cf1eb
Seth Traverse <seth@viaware.ca>
2018-02-10 11:22:25 -0800

    Added test spurt ability

---
+++ Assets/Scripts/LotionParticle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotionParticle : MonoBehaviour {
+
+    public float expiry = 1;
+
+    private float startTime;
+
+	void Start () {
+	    	
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SpurtAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpurtAbility : PlayerAbility
+{
+
+    public Transform emitter;
+    public Transform lotionParticle;
+
+    public int amount = 10;
+    public float speed = 10;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Spurt"))
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                var particle = Instantiate(lotionParticle);
+                var body = particle.GetComponent<Rigidbody2D>();
+                body.position = emitter.position + new Vector3((Random.value - 0.5f) * 0.5f, (Random.value - 0.5f) * 0.5f, 0);
+                body.velocity += new Vector2(-speed, 0);
+                body.angularVelocity += 5f;
+            }
+        }
+    }
+}
