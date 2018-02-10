595a9d4b396fe23e1b28b333543285b87aedf45f
Seth Traverse <seth@viaware.ca>
2018-02-10 11:25:02 -0800

    Added debug interaction handler

---
+++ Assets/Scripts/DebugHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugHandler : InteractionHandler
+{
+    public override void handleAction()
+    {
+        Debug.Log("DEBUG INTERACTION");
+    }
+}
