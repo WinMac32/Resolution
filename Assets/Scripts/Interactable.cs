5857580574bdb7493c9466ed047ce3689c6ea057
Seth Traverse <seth@viaware.ca>
2018-02-10 11:17:10 -0800

    Added basic interactable skeleton

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    public string uiMessage;
+    public string button;
+
+    public Text text;
+    public InteractionHandler handler;
+
+    private bool playerInside;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (playerInside && Input.GetButtonDown(button))
+        {
+            handler.handleAction();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerInside = true;
+        text.text = uiMessage;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerInside = false;
+        text.text = "";
+    }
+}
