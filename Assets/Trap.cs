5e06547222204f80235c3a2e1c3be6a333933aaf
Athilc <gaccount@126.com>
2018-02-11 00:07:57 -0800

    Traps in desert level and level adjestment.

--- Assets/Trap.cs
+++ Assets/Trap.cs
@@ -8,7 +8,16 @@ public class Trap : MonoBehaviour {
     public float DoTPeriod = 0.2f;  // how long will it damage the player for the next time
     public int DoTDamage = 2; // how much damage does it damage the player for each time
     private float currentTime = 0;
+    private float smokeTimer = 0.1f;
+    private float currentSmokeTime = 0.1f;
+    public GameObject[] smokes;
+    private int i = 0;
 	void Start () {
+        foreach(GameObject smoke in smokes)
+        {
+            smoke.SetActive(false);
+        }
+        smokes[i].SetActive(true);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -24,5 +33,24 @@ public class Trap : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         currentTime -= Time.deltaTime;
+
+        if (currentSmokeTime >= 0)
+        {
+            currentSmokeTime -= Time.deltaTime;
+        }
+        else
+        {
+            smokes[i].SetActive(false);
+            if (i < smokes.Length-1)
+            {
+                i++;
+            }
+            else {
+                i = 0;
+            }
+            
+            smokes[i].SetActive(true);
+            currentSmokeTime = smokeTimer;
+        }
 	}
 }
