a4b17fdaba5c5be5c07b766fa81fe032e79a8203
Rhiknow <rhiknow55@gmail.com>
2018-02-11 15:36:27 -0800

    dynamic title screen

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,11 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour {
 
-	public void ClickToStart()
+    public string sceneName;
+    public Image image;
+    public Sprite pushedDown;
+    public Sprite noTwinkle;
+    public Sprite hasTwinkle;
+    private float _lastTime;
+
+    public void ClickToStart()
 	{
-        GameManager.instance.LoadScene("Desert Level");
+        image.sprite = pushedDown;
+        GameManager.instance.LoadScene(sceneName);
+    }
+
+    void Update()
+    {
+        if (Time.time >= _lastTime + 1f)
+        {
+            if (image.sprite == hasTwinkle)
+            {
+                image.sprite = noTwinkle;
+            }
+            else
+            {
+                image.sprite = hasTwinkle;
+            }
+            _lastTime = Time.time;
+        }
     }
 }
