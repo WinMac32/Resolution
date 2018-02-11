1e3e421a5daf9db2f52b01da2d3606396afa690e
Athilc <gaccount@126.com>
2018-02-10 17:45:21 -0800

    elevator, and sperated enemy walking boundary from ground

---
+++ Assets/Scripts/ElevatorControler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorControler : MonoBehaviour {
+
+    public ElevatorControler target;
+    public PlayerController player;
+    public GameObject platfrom;
+    public bool isPlatfromHere;
+    public float CD = 3f;
+    public float currentCD = 0;
+	// Use this for initialization
+	void Start () {
+        if (isPlatfromHere)
+        {
+            platfrom.SetActive(true);
+
+        }
+        else
+        {
+            platfrom.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentCD -= Time.deltaTime;
+        if (isPlatfromHere)
+        {
+            platfrom.SetActive(true);
+        }
+        else
+        {
+            platfrom.SetActive(false);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player")&&currentCD<=0)
+        {
+            player.transform.position = target.transform.position;
+            platfrom.SetActive(false);
+            isPlatfromHere = false;
+            target.isPlatfromHere = true;
+            target.currentCD = CD;
+
+        }
+    }
+}
