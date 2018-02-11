433bdd24dde1889c1dde074ff14c443dad748409
Seth Traverse <seth@viaware.ca>
2018-02-10 18:24:07 -0800

    Added interactables and fountain handler

---
+++ Assets/Scripts/LotionRefillInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotionRefillInteraction : InteractionHandler
+{
+    private LotionManager lotionManager;
+
+    public int lotionRefill = 40;
+
+    public void Start()
+    {
+        lotionManager = GameManager.instance.lotionManager;
+    }
+
+    public override void handleAction()
+    {
+        lotionManager.lotionStash += lotionRefill;
+    }
+}
